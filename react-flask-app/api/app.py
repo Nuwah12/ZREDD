442511013840c60d0801fe7eeaf101894335c806
Nuwah12 <nburget12@gmail.com>
2024-12-06 13:19:32 -0500

    public repo

---
+++ react-flask-app/api/app.py
@@ -0,0 +1,539 @@
+from flask import Flask, request
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+endpoint = os.getenv("DATABASE_ENDPOINT")
+port = os.getenv("DATABASE_PORT")
+dbname = os.getenv("DATABASE_NAME")
+user = os.getenv("DATABASE_USER")
+password = os.getenv("DATABASE_PASSWORD")
+
+db_url = "postgresql://{}:{}@{}:{}/{}".format(user, password, endpoint, port, dbname) # Construct DB URL string
+connection = psycopg2.connect(db_url) # Establish connection to the DB
+connection.autocommit = True
+
+@app.get("/") # Root
+def index():
+	return "CIS 550 Project"
+
+@app.get("/checkdbconnection") # Print the database url
+def db_conn_check():
+	status = ''
+	conn_val = connection.closed # Should be 0 (False) if the connection is not closed (i.e. open)
+	if conn_val == 0:
+		status = 'Alive'
+	else:
+		status = 'Dead'
+	return "DB URL: {} ; Status: {}".format(db_url, status)
+
+########## Accessory routes for frontend lists ##########
+@app.get("/get_localities_zhvi")
+def get_states_zhvi():
+	locality_type = request.args.get("type")
+	page = int(request.args.get("page", 1))  # Default to page 1
+	limit = int(request.args.get("limit", 10))  # Default to 10 records per page
+	offset = (page - 1) * limit  # Calculate OFFSET
+
+	table_name = "zhvi_processed_by_zhvi_{}_cleaned".format(locality_type)
+
+	if locality_type == "metro":
+		locality_type = "msa"
+
+	try:
+		with connection.cursor() as cursor:
+			if locality_type == 'state':
+				query = f'''
+				SELECT DISTINCT regionname, regionname
+				FROM {table_name} zhvi 
+					JOIN "Regions_cleaned" rc ON zhvi.regionid = rc.regionid 
+				WHERE regiontype = %s
+				ORDER BY regionname ASC
+				LIMIT %s OFFSET %s
+				'''
+			else:
+				query = f'''
+				SELECT DISTINCT regionname, statename
+				FROM {table_name} zhvi 
+					JOIN "Regions_cleaned" rc ON zhvi.regionid = rc.regionid 
+				WHERE regiontype = %s
+				ORDER BY regionname ASC
+				LIMIT %s OFFSET %s
+				'''
+			cursor.execute(query, (locality_type, limit, offset))
+			rows = cursor.fetchall()
+
+			cursor.execute(f'SELECT COUNT(DISTINCT regionid) FROM {table_name}')
+			total_count = cursor.fetchone()[0]
+			response = {
+			"options": [{"regionname": row[0], "state": row[1]} for row in rows],  # Adjust based on your table structure
+			"totalPages": (total_count + limit - 1) // limit  # Calculate total pages
+	 		}
+			return response
+	except Exception as e:
+		return {"error": str(e)}, 500
+
+@app.get("/get_localities_zori")
+def get_states_zori():
+	locality_type = request.args.get("type")
+	page = int(request.args.get("page", 1))  # Default to page 1
+	limit = int(request.args.get("limit", 10))  # Default to 10 records per page
+	offset = (page - 1) * limit  # Calculate OFFSET
+
+	table_name = "zori_processed_by_zori_{}_cleaned".format(locality_type)
+
+	if locality_type == "metro":
+		locality_type = "msa"
+
+	try:
+		with connection.cursor() as cursor:
+			query = f'''
+			SELECT DISTINCT regionname, statename
+			FROM {table_name} zori 
+				JOIN "Regions_cleaned" rc ON zori.regionid = rc.regionid 
+			WHERE regiontype = %s
+			ORDER BY regionname ASC
+			LIMIT %s OFFSET %s
+			'''
+			cursor.execute(query, (locality_type, limit, offset))
+			rows = cursor.fetchall()
+
+			cursor.execute(f'SELECT COUNT(DISTINCT regionid) FROM {table_name}')
+			total_count = cursor.fetchone()[0]
+			response = {
+			"options": [{"regionname": row[0], "state": row[1]} for row in rows],  # Adjust based on your table structure
+			"totalPages": (total_count + limit - 1) // limit  # Calculate total pages
+	 		}
+			return response
+	except Exception as e:
+		return {"error": str(e)}, 500
+
+@app.get("/get_localities_zhvf")
+def get_states_zhvf():
+	locality_type = request.args.get("type")
+	page = int(request.args.get("page", 1))  # Default to page 1
+	limit = int(request.args.get("limit", 10))  # Default to 10 records per page
+	offset = (page - 1) * limit  # Calculate OFFSET
+
+	table_name = "zhvf_by_zhvf_{}_cleaned".format(locality_type)
+
+	if locality_type == "metro":
+		locality_type = "msa"
+
+	try:
+		with connection.cursor() as cursor:
+			query = f'''
+			SELECT DISTINCT regionname, statename
+			FROM {table_name} zhvf 
+				JOIN "Regions_cleaned" rc ON zhvf.regionid = rc.regionid 
+			WHERE regiontype = %s
+			ORDER BY regionname ASC
+			LIMIT %s OFFSET %s
+			'''
+			cursor.execute(query, (locality_type, limit, offset))
+			rows = cursor.fetchall()
+
+			cursor.execute(f'SELECT COUNT(DISTINCT regionid) FROM {table_name}')
+			total_count = cursor.fetchone()[0]
+			response = {
+			"options": [{"regionname": row[0], "state": row[1]} for row in rows],  # Adjust based on your table structure
+			"totalPages": (total_count + limit - 1) // limit  # Calculate total pages
+	 		}
+			return response
+	except Exception as e:
+		return {"error": str(e)}, 500
+
+@app.get("/get_localities_mhi")
+def get_states_mhi():
+	locality_type = request.args.get("type")
+	page = int(request.args.get("page", 1))  # Default to page 1
+	limit = int(request.args.get("limit", 10))  # Default to 10 records per page
+	offset = (page - 1) * limit  # Calculate OFFSET
+
+	if locality_type == "metro":
+		locality_type = "msa"
+	rows=[]
+	total_count=0
+	try:
+		with connection.cursor() as cursor:
+			# We can't just access different tables here, so we will have to have different queries
+			if locality_type == 'msa':
+				query = f'''
+				SELECT DISTINCT regionname, statename
+				FROM mhi_processed_by_metro_cleaned mhi 
+					JOIN "Regions_cleaned" rc ON mhi.regionid = rc.regionid 
+				WHERE regiontype = %s
+				ORDER BY regionname ASC
+				LIMIT %s OFFSET %s
+				'''
+				cursor.execute(query, (locality_type, limit, offset))
+				rows = cursor.fetchall()
+				print(rows)
+				cursor.execute(f'SELECT COUNT(DISTINCT regionid) FROM mhi_processed_by_metro_cleaned')
+				total_count = cursor.fetchone()[0]
+
+			elif locality_type == 'state':
+				query = f'''
+				SELECT DISTINCT statename, statename
+				FROM mhi_processed_by_metro_cleaned mhi
+					JOIN "Regions_cleaned" rc ON mhi.regionid = rc.regionid
+				ORDER BY statename ASC
+				LIMIT %s OFFSET %s
+				'''
+				cursor.execute(query, (limit, offset))
+				rows = cursor.fetchall()
+				print(rows)
+				cursor.execute(f'SELECT COUNT(DISTINCT statename) FROM mhi_processed_by_metro_cleaned mhi JOIN "Regions_cleaned" rc ON mhi.regionid = rc.regionid ')
+				total_count = cursor.fetchone()[0]
+			response = {
+			"options": [{"regionname": row[0], "state": row[1]} for row in rows],  # Adjust based on your table structure
+			"totalPages": (total_count + limit - 1) // limit  # Calculate total pages
+	 		}
+		return response
+	except Exception as e:
+		return {"error": str(e)}, 500
+
+@app.get("/get_localities_sales")
+def get_states_sales():
+	locality_type = request.args.get("type")
+	page = int(request.args.get("page", 1))  # Default to page 1
+	limit = int(request.args.get("limit", 10))  # Default to 10 records per page
+	offset = (page - 1) * limit  # Calculate OFFSET
+
+	if locality_type == "metro":
+		locality_type = "msa"
+
+	try:
+		with connection.cursor() as cursor:
+			if locality_type == 'msa':
+				query = f'''
+				SELECT DISTINCT regionname, statename
+				FROM sales_processed_by_metro_cleaned sales 
+					JOIN "Regions_cleaned" rc ON sales.regionid = rc.regionid 
+				WHERE regiontype = '{locality_type}'
+				ORDER BY regionname ASC
+				LIMIT {limit} OFFSET {offset}
+				'''
+			elif locality_type == 'state':
+				query = f'''
+				SELECT DISTINCT statename, statename
+				FROM sales_processed_by_metro_cleaned sales
+					JOIN "Regions_cleaned" rc ON sales.regionid = rc.regionid
+				ORDER BY statename ASC
+				LIMIT {limit} OFFSET {offset}
+				'''
+			cursor.execute(query)
+			rows = cursor.fetchall()
+
+			cursor.execute('SELECT COUNT(DISTINCT regionid) FROM sales_processed_by_metro_cleaned')
+			total_count = cursor.fetchone()[0]
+			response = {
+			"options": [{"regionname": row[0], "state": row[1]} for row in rows],  # Adjust based on your table structure
+			"totalPages": (total_count + limit - 1) // limit  # Calculate total pages
+	 		}
+			return response
+	except Exception as e:
+		return {"error": str(e)}, 500
+
+@app.get("/get_localities_newConstructionSales")
+def get_states_newConstructionSales():
+	locality_type = request.args.get("type")
+	page = int(request.args.get("page", 1))  # Default to page 1
+	limit = int(request.args.get("limit", 10))  # Default to 10 records per page
+	offset = (page - 1) * limit  # Calculate OFFSET
+
+	if locality_type == "metro":
+		locality_type = "msa"
+
+	try:
+		with connection.cursor() as cursor:
+			if locality_type == 'msa':
+				query = f'''
+				SELECT DISTINCT regionname, statename
+				FROM newconsales_processed_by_metro_cleaned sales 
+					JOIN "Regions_cleaned" rc ON sales.regionid = rc.regionid 
+				WHERE regiontype = '{locality_type}'
+				ORDER BY regionname ASC
+				LIMIT {limit} OFFSET {offset}
+				'''
+			elif locality_type == 'state':
+				query = f'''
+				SELECT DISTINCT statename, statename
+				FROM newconsales_processed_by_metro_cleaned sales
+					JOIN "Regions_cleaned" rc ON sales.regionid = rc.regionid
+				ORDER BY statename ASC
+				LIMIT {limit} OFFSET {offset}
+				'''
+			cursor.execute(query)
+			rows = cursor.fetchall()
+
+			cursor.execute('SELECT COUNT(DISTINCT regionid) FROM newconsales_processed_by_metro_cleaned')
+			total_count = cursor.fetchone()[0]
+			response = {
+			"options": [{"regionname": row[0], "state": row[1]} for row in rows],  # Adjust based on your table structure
+			"totalPages": (total_count + limit - 1) // limit  # Calculate total pages
+	 		}
+			return response
+	except Exception as e:
+		return {"error": str(e)}, 500
+
+
+#####
+# Get ZHVI data by locality type and name
+# Example URL: localhost:5000/get_zhvi?type=state&name=%27Pennsylvania%27 will get ZHVI data for Pennsylvania
+#####
+@app.get("/get_zhvi")
+def get_zhvi():
+	locality_type = request.args.get('type')
+	locality_name = request.args.get('name')
+	state_name = request.args.get('state')
+	
+	valid_locality_types = ['state', 'county', 'metro', 'city', 'zip', 'neighborhood']
+	if locality_type not in valid_locality_types:
+		return {"error": "Invalid locality type"}, 400
+
+	table_name = "zhvi_processed_by_zhvi_{}_cleaned".format(locality_type)
+
+	if locality_type == 'metro':
+		locality_type = 'msa'
+
+	with connection:
+		with connection.cursor() as cursor:
+			print(locality_type)
+			if locality_type == 'state': # In this case, the statename columns are null
+				query = f'''
+                SELECT date, regionname, regiontype, value AS ZHVI
+				FROM {table_name} zhvi
+				JOIN "Regions_cleaned" rc ON zhvi.regionid = rc.regionid
+				WHERE regionname = '{locality_name}' AND regiontype = '{locality_type}'
+				ORDER BY date ASC
+           		'''
+			else:
+				query = f'''
+	                SELECT date, regionname, regiontype, value AS ZHVI
+					FROM {table_name} zhvi
+					JOIN "Regions_cleaned" rc ON zhvi.regionid = rc.regionid
+					WHERE regionname = '{locality_name}' AND regiontype = '{locality_type}' AND rc.statename = '{state_name}'
+					ORDER BY date ASC
+	            '''
+			print(query)
+			cursor.execute(query)
+			rows = cursor.fetchall()
+			# Get column names
+			col_names = [desc[0] for desc in cursor.description]
+			# Convert to list of dictionaries for JSON response
+			result = [dict(zip(col_names, row)) for row in rows]
+			return result
+
+#####
+# Get ZORI data by locality type and name
+# Example URL: localhost:5000/get_zori?type=state&name=%27Pennsylvania%27 will get ZORI data for Pennsylvania
+#####
+@app.get("/get_zori")
+def get_zori():
+	locality_type = request.args.get('type')
+	locality_name = request.args.get('name')
+	state_name = request.args.get('state')
+	
+	valid_locality_types = ['state', 'county', 'metro', 'city', 'zip', 'neighborhood']
+	if locality_type not in valid_locality_types:
+		return {"error": "Invalid locality type"}, 400
+
+	table_name = "zori_processed_by_zori_{}_cleaned".format(locality_type)
+
+	if locality_type == 'metro':
+		locality_type = 'msa'
+
+	print(table_name)
+	with connection:
+		with connection.cursor() as cursor:
+			query = f'''
+                SELECT date, regionname, regiontype, value AS ZORI
+				FROM {table_name} zori
+				JOIN "Regions_cleaned" rc ON zori.regionid = rc.regionid
+				WHERE regionname = '{locality_name}' AND regiontype = '{locality_type}' AND statename = '{state_name}'
+				ORDER BY date ASC
+            '''
+			print(query)
+			cursor.execute(query)
+			rows = cursor.fetchall()
+			# Get column names
+			col_names = [desc[0] for desc in cursor.description]
+			# Convert to list of dictionaries for JSON response
+			result = [dict(zip(col_names, row)) for row in rows]
+			return result
+
+#####
+# Get ZHVF data by locality type and name
+# Example URL: localhost:5000/get_zhvf?type=state&name=%27Pennsylvania%27 will get ZHVF data for Pennsylvania
+#####
+@app.get("/get_zhvf")
+def get_zhvf():
+	locality_type = request.args.get('type')
+	locality_name = request.args.get('name')
+	
+	valid_locality_types = ['state', 'county', 'metro', 'city', 'zip', 'neighborhood']
+	if locality_type not in valid_locality_types:
+		return {"error": "Invalid locality type"}, 400
+
+	table_name = "zhvf_by_zhvf_{}_cleaned".format(locality_type)
+
+	if locality_type == 'metro':
+		locality_type = 'msa'
+
+	with connection:
+		with connection.cursor() as cursor:
+			query = f'''
+                SELECT month, quarter, year
+				FROM {table_name} zhvf
+				JOIN "Regions_cleaned" rc ON zhvf.regionid = rc.regionid
+				WHERE regionname = '{locality_name}' AND regiontype = '{locality_type}'
+				ORDER BY basedate ASC
+            '''
+			print(query)
+			cursor.execute(query)
+			rows = cursor.fetchall()
+			# Get column names
+			col_names = [desc[0] for desc in cursor.description]
+			# Convert to list of dictionaries for JSON response
+			result = [dict(zip(col_names, row)) for row in rows]
+			print(result)
+			return result
+
+#####
+# Get MHI data by locality type and name
+# Example URL: localhost:5000/get_zori?type=state&name=%27Pennsylvania%27 will get ZORI data for Pennsylvania
+#####
+@app.get("/get_mhi")
+def get_mhi():
+	locality_type = request.args.get('type')
+	locality_name = request.args.get('name')
+	
+	valid_locality_types = ['state', 'metro']
+	if locality_type not in valid_locality_types:
+		return {"error": "Invalid locality type"}, 400
+
+	if locality_type == 'metro':
+		locality_type == 'msa'
+
+	with connection:
+		with connection.cursor() as cursor:
+			if locality_type == 'metro':
+				query = f'''
+					SELECT rc.regionname, date, value AS mhi
+					FROM mhi_processed_by_metro_cleaned mhi
+					JOIN "Regions_cleaned" rc ON mhi.regionid = rc.regionid
+					WHERE regionname= '{locality_name}'
+					ORDER BY date ASC
+	            '''
+	        # Since there is only a metro table for MHI, we can estimate the by-State values by averaging values for all metros in a state using a GROUP BY clause with AVG()
+			elif locality_type == 'state':
+				query = f'''
+					SELECT statename, date, AVG(CAST(value AS float)) AS mhi
+					FROM mhi_processed_by_metro_cleaned mhi
+					JOIN "Regions_cleaned" rc ON mhi.regionid = rc.regionid
+					WHERE statename = '{locality_name}'
+					GROUP BY statename, date
+					ORDER BY date ASC
+	            '''
+			print(query)
+			cursor.execute(query)
+			rows = cursor.fetchall()
+			# Get column names
+			col_names = [desc[0] for desc in cursor.description]
+			# Convert to list of dictionaries for JSON response
+			result = [dict(zip(col_names, row)) for row in rows]
+			return result
+
+#####
+# Get home sale count data by locality type and name
+#####
+@app.get("/get_homesales")
+def get_homesales():
+	locality_type = request.args.get('type')
+	locality_name = request.args.get('name')
+	
+	valid_locality_types = ['state', 'metro']
+	if locality_type not in valid_locality_types:
+		return {"error": "Invalid locality type"}, 400
+
+	if locality_type == 'metro':
+		locality_type == 'msa'
+
+	with connection:
+		with connection.cursor() as cursor:
+			if locality_type == 'metro':
+				query = f'''
+					SELECT rc.regionname, date, value AS count
+					FROM sales_processed_by_metro_cleaned sales
+					JOIN "Regions_cleaned" rc ON sales.regionid = rc.regionid
+					WHERE regionname= '{locality_name}'
+					ORDER BY date ASC
+		         	'''
+		    # Similar to above, get average home sales for all metros in a state
+			elif locality_type == 'state':
+				query = f'''
+					SELECT statename, date, AVG(CAST(value AS float)) AS count
+					FROM sales_processed_by_metro_cleaned sales
+					JOIN "Regions_cleaned" rc ON sales.regionid = rc.regionid
+					WHERE statename = '{locality_name}'
+					GROUP BY statename, date
+					ORDER BY date ASC
+		         	'''
+			print(query)
+			cursor.execute(query)
+			rows = cursor.fetchall()
+			# Get column names
+			col_names = [desc[0] for desc in cursor.description]
+			# Convert to list of dictionaries for JSON response
+			result = [dict(zip(col_names, row)) for row in rows]
+			return result
+
+#####
+# Get new construction sale count data by locality type and name
+#####
+@app.get("/get_newConstructionSales")
+def get_newConstructionSales():
+	locality_type = request.args.get('type')
+	locality_name = request.args.get('name')
+	
+	valid_locality_types = ['state', 'metro']
+	if locality_type not in valid_locality_types:
+		return {"error": "Invalid locality type"}, 400
+
+	if locality_type == 'metro':
+		locality_type == 'msa'
+
+	with connection:
+		with connection.cursor() as cursor:
+			if locality_type == 'metro':
+				query = f'''
+					SELECT rc.regionname, date, value AS count
+					FROM newconsales_processed_by_metro_cleaned sales
+					JOIN "Regions_cleaned" rc ON sales.regionid = rc.regionid
+					WHERE regionname= '{locality_name}'
+					ORDER BY date ASC
+		         	'''
+		    # Similar to above, get average home sales for all metros in a state
+			elif locality_type == 'state':
+				query = f'''
+					SELECT statename, date, AVG(CAST(value AS float)) AS count
+					FROM newconsales_processed_by_metro_cleaned sales
+					JOIN "Regions_cleaned" rc ON sales.regionid = rc.regionid
+					WHERE statename = '{locality_name}'
+					GROUP BY statename, date
+					ORDER BY date ASC
+		         	'''
+			print(query)
+			cursor.execute(query)
+			rows = cursor.fetchall()
+			# Get column names
+			col_names = [desc[0] for desc in cursor.description]
+			# Convert to list of dictionaries for JSON response
+			result = [dict(zip(col_names, row)) for row in rows]
+			return result
+
---
+++ react-flask-app/src/App.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ZHVI from './pages/zhvi';
+import ZORI from './pages/zori';
+import ZHVF from './pages/zhvf';
+import MHI from './pages/mhi';
+import HomeSales from './pages/homesales'
+import NewConstruction from './pages/newConstructionSales';
+//import Sales from './pages/Sales';
+import './App.css';
+function App() {
+
+  const [selectedLocalityType, setSelectedLocalityType] = useState('');
+
+  const handleLocalityTypeSelect = (type) => {
+    console.log('Selected locality type:', type); // Debugging
+    setSelectedLocalityType(type); // Update selected locality type
+  };
+
+  return (
+<Router>
+      <div className="App">
+        <header>
+          <h1>Zillow Real Estate Data Dashboard</h1>
+          <nav>
+            <div className="dropdown">
+              <Link to="/zhvi" className="nav-link">Zillow Home Value Index (ZHVI)</Link>
+              <div className="dropdown-menu">
+                {/* Pass in value of selectedLocalityType */}
+                <Link to="/zhvi" onClick={() => handleLocalityTypeSelect('state')}>State</Link>
+                <Link to="/zhvi" onClick={() => handleLocalityTypeSelect('metro')}>Metro</Link>
+                <Link to="/zhvi" onClick={() => handleLocalityTypeSelect('county')}>County</Link>
+                <Link to="/zhvi" onClick={() => handleLocalityTypeSelect('city')}>City</Link>
+                <Link to="/zhvi" onClick={() => handleLocalityTypeSelect('zip')}>ZIP</Link>
+              </div>
+            </div>
+            <div className="dropdown">
+              <Link to="/zori" className="nav-link">Zillow Observed Rent Index (ZORI)</Link>
+              <div className="dropdown-menu">
+                <Link to="/zori" onClick={() => handleLocalityTypeSelect('county')}>County</Link>
+                <Link to="/zori" onClick={() => handleLocalityTypeSelect('metro')}>Metro</Link>
+                <Link to="/zori" onClick={() => handleLocalityTypeSelect('city')}>City</Link>
+                <Link to="/zori" onClick={() => handleLocalityTypeSelect('zip')}>ZIP</Link>
+              </div>
+            </div>
+            <div className="dropdown">
+              <Link to="/zhvi" className="nav-link">Zillow Home Value Forecast (ZHVF)</Link>
+              <div className="dropdown-menu">
+                {/* Pass in value of selectedLocalityType */}
+                <Link to="/zhvf" onClick={() => handleLocalityTypeSelect('metro')}>Metro</Link>
+                <Link to="/zhvf" onClick={() => handleLocalityTypeSelect('zip')}>ZIP</Link>
+              </div>
+            </div>
+            <div className="dropdown">
+              <Link to="/mhi" className="nav-link">Market Heat Index (MHI)</Link>
+              <div className="dropdown-menu">
+                <Link to="/mhi" onClick={() => handleLocalityTypeSelect('state')}>State</Link>
+                <Link to="/mhi" onClick={() => handleLocalityTypeSelect('metro')}>Metro</Link>
+              </div>
+            </div>
+            <div className="dropdown">
+              <Link to="/homesales" className="nav-link">Home Sales</Link>
+              <div className="dropdown-menu">
+                {/* Pass in value of selectedLocalityType */}
+                <Link to="/homesales" onClick={() => handleLocalityTypeSelect('state')}>State</Link>
+                <Link to="/homesales" onClick={() => handleLocalityTypeSelect('metro')}>Metro</Link>
+              </div>
+            </div>
+            <div className="dropdown">
+              <Link to="/newConstructionSales" className="nav-link">New Construction Sales</Link>
+              <div className="dropdown-menu">
+                {/* Pass in value of selectedLocalityType */}
+                <Link to="/newConstructionSales" onClick={() => handleLocalityTypeSelect('state')}>State</Link>
+                <Link to="/newConstructionSales" onClick={() => handleLocalityTypeSelect('metro')}>Metro</Link>
+              </div>
+            </div>
+            <div className="dropdown">
+              <Link to="/zhvi" className="nav-link">For-Sale Listings</Link>
+              <div className="dropdown-menu">
+                {/* Pass in value of selectedLocalityType */}
+                <Link to="/zhvi" onClick={() => handleLocalityTypeSelect('metro')}>Metro</Link>
+              </div>
+            </div>
+          </nav>
+
+        </header>
+        <main>
+          <Routes>
+            {/* Pass selectedLocalityType to ZHVI */}
+            <Route path="/zhvi" element={<ZHVI localityType={selectedLocalityType} />} />
+            <Route path="/zori" element={<ZORI localityType={selectedLocalityType} />} />
+            <Route path="/zhvf" element={<ZHVF localityType={selectedLocalityType} />} />
+            <Route path="/mhi" element={<MHI localityType={selectedLocalityType} />} />
+            <Route path="/homesales" element={<HomeSales localityType={selectedLocalityType} />} />
+            <Route path="/newConstructionSales" element={<NewConstruction localityType={selectedLocalityType} />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
